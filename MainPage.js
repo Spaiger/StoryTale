b7dbb75845df3b785a605f859ed7026219343a56
lsn03 <graffgray8@gmail.com>
2022-12-27 13:08:20 +1000

    fix mobile menu

--- MainPage.js
+++ MainPage.js
@@ -1,13 +1,13 @@
 function menu(){
-    document.getElementById("conn").style.display = "none";
-    document.getElementById("mb").style.display = "none";
+    document.getElementById("conn").style.display = "none !important";
+    document.getElementById("mb").style.display = "none !important";
     document.getElementById("butmenu").style.display = "block";
 }
 
 function clo(){
     document.getElementById("butmenu").style.display = "none";
     document.getElementById("conn").style.display = "flex";
-    document.getElementById("mb").style.display = "none";
+    document.getElementById("mb").style.display = "flex";
 }
 
 // function chan(){
